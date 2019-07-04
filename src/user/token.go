64bc6fc545180f95aea0177e8f68dbf0cd638075
kdm <kdm@xiongwaideMacBook-Pro.local>
2019-07-04 15:11:24 +0800

    修改提示

--- src/user/token.go
+++ src/user/token.go
@@ -12,39 +12,44 @@ import (
 // 验证token
 func ValidateToken(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		// 获取token字符串
 		tokenStr := c.Request().Header.Get("token")
 		if tokenStr == "" {
 			return c.JSON(200, util.ReturnBody(1, "", "token不能为空！"))
 		}
+		// 解密token
 		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
 			return []byte("secret"), nil
 		})
 		if err != nil {
 			return c.JSON(200, util.ReturnBody(1, "", "token解析失败"))
 		}
+		// 获取token内容
 		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 			userId := claims["userId"]
 			expDate := claims["expDate"]
 			timeNow := time.Now().Format("2006-01-02 15:04:05")
-			c.Request().Header.Set("userId", userId.(string))
 			if (timeNow > expDate.(string)) {
 				return c.JSON(200, util.ReturnBody(1, "", "token已失效，请重新获取"))
 			}
 			if userId == "" {
 				return c.JSON(200, util.ReturnBody(1, "", "token不合法"))
 			}
+			// 将用户id放入请求头
+			c.Request().Header.Set("userId", userId.(string))
 		} else {
 			return c.JSON(200, util.ReturnBody(1, "", "token解析失败"))
 		}
+		
 		return next(c)
 	}
 }
 
 // 生成Token
 func MakeToken(name string, id bson.ObjectId) string {
-	// Create token
+	// 生成token
 	token := jwt.New(jwt.SigningMethodHS256)
-	// Set claims
+	// 绑定数据
 	claims := token.Claims.(jwt.MapClaims)
 	claims["name"] = name
 	claims["userId"] = id.Hex()
